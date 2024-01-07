747a24217f7f551e43d2c7ac6c198abb5da407e3
world <world@unseen.dev>
2024-01-07 01:07:54 -0500

    Pass in secrets files when running setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# Use nullglob in case there are no matching files
+shopt -s nullglob
 
 if ! command -v ansible-playbook &> /dev/null
 then
@@ -6,4 +8,18 @@ then
   exit 1
 fi
 
-ansible-playbook "$@" dotfiles.yaml
+# Include all YAML files from `secrets` folder as extra variables
+secret_files=(secrets/*.yaml)
+secret_files+=(secrets/*.yml)
+secret_file_args="${secret_files[@]/#/-e @}"
+
+# Ask for vault password if secret files exist
+ask_vault_pass_flag=""
+if [ -n "${secret_file_args}" ]
+then
+  ask_vault_pass_flag="--ask-vault-pass"
+fi
+
+command="ansible-playbook $@ ${secret_file_args} ${ask_vault_pass_flag} dotfiles.yaml"
+echo "${command}"
+eval -- "${command}"
